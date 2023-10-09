863d03870aa4d2ea801c9c99776913b3b6d453b4
Arseniy Arsentyev <programpro.ars@gmail.com>
2023-10-09 09:11:47 -0400

    Add files via upload

--- audioextract.py
+++ audioextract.py
@@ -7,18 +7,18 @@ def download_audio(link, directory=""):
     if link.find("playlist") != -1:
         playlist = Playlist(link)
         os.mkdir(playlist.title)
-        print("Downloading Playlist (" + str(playlist.length) + " videos):\n")
+        print("\nDownloading Playlist (" + str(playlist.length) + " videos):\n")
         i = 1
         for video in playlist.videos:
             (video.streams.filter(mime_type="audio/mp4", only_audio=True)[-1]
              .download(os.path.join(directory, playlist.title)))
             print("Audio from", video.title, "was extracted")
             i += 1
-        print("\nAll audios were successfully extracted.")
+        print("\nAll audios were successfully extracted.\n")
     else:
         video = YouTube(link)
         video.streams.filter(mime_type="audio/mp4", only_audio=True)[-1].download(directory)
-        print("Audio from", video.title, "was successfully extracted.")
+        print("\nAudio from", video.title, "was successfully extracted.\n")
 
 
 if __name__ == "__main__":
